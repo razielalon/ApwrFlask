9e44f35da89ac5d1ec4bc982a2209350815861b8
Raziel <razielalon@gmail.com>
2024-11-03 11:38:18 +0200

    changing headline faults

--- application.py
+++ application.py
@@ -5,8 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def home():  # put application's code here
-    return '<h1>Hello World, i am changing from my loacl machine again </h1>'
-
+    return '<h1>Hello World, I am changing from my local machine again </h1>'
 
 
 if __name__ == '__main__':
